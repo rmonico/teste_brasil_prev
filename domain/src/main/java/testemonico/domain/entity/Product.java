e962f339fbdc0f71d389fe0ec0ebb8205eee653e
Rafael Monico <rmonico@moninburgo>
2020-07-03 04:51:36 -0300

    Product using lombok builder

--- domain/src/main/java/testemonico/domain/entity/Product.java
+++ domain/src/main/java/testemonico/domain/entity/Product.java
@@ -1,10 +1,12 @@
 package testemonico.domain.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class Product implements BaseEntity {
 
     private String id;
