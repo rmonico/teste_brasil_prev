627748aa639c001202131970d3985c7810cf39ab
Rafael Monico <rmonico@moninburgo>
2020-07-06 04:47:02 -0300

    ProductController implemented

--- adapter/controller/src/main/java/testemonico/controller/product/ProductController.java
+++ adapter/controller/src/main/java/testemonico/controller/product/ProductController.java
@@ -3,7 +3,15 @@ package testemonico.controller.product;
 import lombok.Builder;
 import testemonico.controller.product.model.ProductRequest;
 import testemonico.controller.product.model.ProductResponse;
+import testemonico.domain.entity.Product;
 import testemonico.usecase.product.crud.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
 
 @Builder
 public class ProductController {
@@ -17,5 +25,25 @@ public class ProductController {
         var product = request.toProduct();
 
         return new ProductResponse(createProduct.create(product));
+    }
+
+    public ProductResponse updateProduct(ProductRequest request) {
+        var product = request.toProduct();
+
+        return new ProductResponse(updateProduct.update(product));
+    }
+
+    public ProductResponse deleteProduct(String id) {
+        return new ProductResponse(deleteProduct.delete(id));
+    }
+
+    public List<ProductResponse> findAllProducts() {
+        return findAllProducts.findAll().stream().map(ProductResponse::new).collect(Collectors.toList());
+    }
+
+    public ProductResponse findProductById(String id) {
+        Product rawProduct = findProductById.findById(id).orElseThrow(() -> new RuntimeException(format("Product not found (%s)", id)));
+
+        return new ProductResponse(rawProduct);
     }
 }
