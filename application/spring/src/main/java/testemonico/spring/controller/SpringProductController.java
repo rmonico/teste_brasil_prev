b813b1da63e9bed9d23fd89685497c5ec273c469
Rafael Monico <rmonico@moninburgo>
2020-07-06 04:45:29 -0300

    SpringController renamed to SpringProductController

---
+++ application/spring/src/main/java/testemonico/spring/controller/SpringProductController.java
@@ -0,0 +1,58 @@
+package testemonico.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import testemonico.controller.product.ProductController;
+import testemonico.controller.product.model.ProductRequest;
+import testemonico.controller.product.model.ProductResponse;
+
+import java.util.List;
+
+@RestController("/api/v1/product")
+// TODO Fix returned status codes for all methods
+public class SpringProductController {
+
+    private final ProductController productController;
+
+    @Autowired
+    public SpringProductController(ProductController productController) {
+        this.productController = productController;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(@RequestBody ProductRequest request) {
+        ProductResponse response = productController.createProduct(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@RequestBody String id) {
+        ProductResponse response = productController.deleteProduct(id);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateProduct(@RequestBody ProductRequest request) {
+        ProductResponse response = productController.updateProduct(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    // TODO Paging
+    @GetMapping
+    public ResponseEntity<?> findAllProducts() {
+        List<ProductResponse> response = productController.findAllProducts();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findProductById(@RequestParam String id) {
+        ProductResponse response = productController.findProductById(id);
+
+        return ResponseEntity.ok(response);
+    }
+}
